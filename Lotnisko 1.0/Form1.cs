30388fe04523d9db4099e9c9c075bbe7893646b4
FilipHojan <81771162+FilipHojan@users.noreply.github.com>
2023-01-03 21:09:29 +0100

    1 zmiana

--- Lotnisko 1.0/Form1.cs
+++ Lotnisko 1.0/Form1.cs
@@ -17,9 +17,57 @@ namespace Lotnisko_1._0
             InitializeComponent();
         }
 
+        private DataTable ReadExcel(string fileName)
+        {
+            IronXL.WorkBook workbook = IronXL.WorkBook.Load(fileName);
+            IronXL.WorkSheet sheet = workbook.DefaultWorkSheet;
+            return sheet.ToDataTable(true);
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                string fileExt = Path.GetExtension(file.FileName);
+                if (fileExt.CompareTo(".xls") == 0 || fileExt.CompareTo(".xlsx") == 0)
+                {
+                    try
+                    {
+                        DataTable dtExcel = ReadExcel(file.FileName);
+                        dataGridView1.Visible = true;
+                        dataGridView1.DataSource = dtExcel;
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(ex.Message.ToString());
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Proszę wybrać pliki z rozszerzeniem .xls or .xlsx file only.", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
     }
 }
